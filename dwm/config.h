7ad20439a0a71cd0cc11dc3e46e738fd6ea45ef3
NexushasTaken <nexushastaken@gmail.com>
2022-12-05 16:02:58 +0800

    Mon Dec  5 04:02:58 PM +08 2022

--- dwm/config.h
+++ dwm/config.h
@@ -137,13 +137,6 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
   { MODKEY,                       XK_h,      shiftview,      {.i = -1}},
   { MODKEY,                       XK_l,      shiftview,      {.i =  1}},
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
